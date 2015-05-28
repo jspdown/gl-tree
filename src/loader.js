07e40b08b1cad1d298290571a0c498d2302ef0e0
jspdown <harold.ozouf@tryon.io>
2015-05-28 21:04:55 +0200

    Removed painful concats

--- src/loader.js
+++ src/loader.js
@@ -6,11 +6,13 @@ function Loader() {
 }
 
 Loader.prototype.load = function (path) {
-  var _this = this,
-      obj = {
+  var _this = this;
+  
+  var obj = {
     vertices: [],
     normals: [],
-    faces: []
+    vertexIndex: [],
+    normalIndex: []
   };
   
   fs.readFileSync(path, 'utf8')
@@ -22,34 +24,41 @@ Loader.prototype.load = function (path) {
 };
 
 Loader.prototype._parseLine = function (obj, line) {
-  var str = line.split(' ');
+  var elems = line.split(' ');
   
-  switch (str[0]) {
-    case 'v':
-      obj.vertices = obj.vertices.concat(this._getVertex(str.slice(1)));
-      break;
-    case 'vn':
-      obj.normals = obj.normals.concat(this._getNormal(str.slice(1)));
-      break;
-    case 'f':
-      obj.faces = obj.faces.concat(this._getFace(str.slice(1)));
-      break;
+  switch (elems[0]) {
+    case 'v':   this._parseVertex(obj, elems.slice(1));
+    break;
+    case 'vn':  this._getNormal(obj, elems.slice(1));
+    break;
+    case 'f':   this._getFace(obj, elems.slice(1));
+    break;
   }
 };
 
-Loader.prototype._getVertex = function (elems) {
-  return elems.map(parseInt);
+Loader.prototype._flatten = function () {
+  console.log('flatten');
 };
-Loader.prototype._getNormal = Loader.prototype._getVertex;
 
-Loader.prototype._getFace = function (elems) {
-  return elems.map(function (elem) {
-    var indexes = elem.split('/');
-    return {
-      vertex: parseInt(indexes[0]),
-      normal: parseInt(indexes[2])
-    };
-  });
+Loader.prototype._parseVertex = function (obj, elems) {
+  obj.vertices.push(elems.map(parseInt));
+};
+
+Loader.prototype._parseNormal = function (obj, elems) {
+  obj.normals.push(elems.map(parseInt));
+};
+
+Loader.prototype._parseFace = function (obj, elems) {
+  var vertex = [],
+      normal = [];
+      
+  elems.forEach(function (elem) {
+    var e = elem.split('/');
+    vertex.push(parseInt(e[0]));
+    normal.push(parseInt(e[2]));
+  }); 
+  obj.vertexIndex.push(vertex);
+  obj.normalIndex.push(vertex);
 };
 
 module.exports = Loader;
